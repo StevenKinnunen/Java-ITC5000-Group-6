e9ec76f157ffb5191d42b1444ad363143db8ecfb
Steven Kinnunen <71653031+StevenKinnunen@users.noreply.github.com>
2021-11-15 19:44:31 -0500

    Update HumberCollegeAdmissions.java

--- HumberCollegeAdmissions.java
+++ HumberCollegeAdmissions.java
@@ -4,5 +4,13 @@ public class HumberCollegeAdmissions {
 	public static void main(String args[]) {
 		
 		System.out.println("Welcome to Humber College");
+		
+		Scanner input = new Scanner(System.in);
+	
+		System.out.println("Please enter a username: ");
+		
+		String username = input.next();
+		
+		System.out.println("Thank you " + username + ", please enter a password:");
 	}
 }
